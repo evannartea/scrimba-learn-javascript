ab61bdb22822e01ec0c762c383d472cd56ed97e0
Evan Nartea <evannartea@Mac.lan>
2026-08-11 13:59:00 +0100

    finish 1.13

--- 1. Build a Passenger Counter App/13. Increment on clicks/1.13_index.js
+++ 1. Build a Passenger Counter App/13. Increment on clicks/1.13_index.js
@@ -1,14 +1,19 @@
-// document.getElementById("count").innerText = 5
+//document.getElementById("count").innerText = 5
 
 // let count = 5
 // count = count + 1
 // console.log(count)
 
 // intialize the count as 0
-// listen for clicks on the increment button
+let count = 0
+
+// listen for clicks on the increment button in HTML
+
 // increment the count variable when the button is clicked
-// change the count-el in the HTML to reflect the new count
+function increment() {
+    count++
+    console.log(count)
+}
 
-
-
+increment()
 
