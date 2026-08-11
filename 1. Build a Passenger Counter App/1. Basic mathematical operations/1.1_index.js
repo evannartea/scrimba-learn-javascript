a554e13e9c47e9d6787ba120e1f56a3a0793b967
Evan Nartea <evannartea@Mac.lan>
2026-08-11 11:13:30 +0100

    changed structure of folders

---
+++ 1. Build a Passenger Counter App/1. Basic mathematical operations/1.1_index.js
@@ -0,0 +1,18 @@
+// document.getElementById("count").innerText = 5
+
+// let firstBatch = 5
+// let secondBatch = 7
+// let count = firstBatch + secondBatch
+// console.log(count)
+
+
+// 1. Create two variables, myAge and humanDogRatio
+let myAge = 26
+let humanDogRatio = 7
+
+// 2. Multiply the two together and store the result in myDogAge 
+myDogAge = myAge * humanDogRatio
+
+// 3. Log myDogAge to the console
+console.log(myDogAge)
+
