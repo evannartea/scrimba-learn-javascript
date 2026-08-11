2902618412b78bb9bac9b85d6dfd5c2fdd2d20ef
Evan Nartea <evannartea@Mac.lan>
2026-08-11 14:15:21 +0100

    finish 1.15

--- 1. Build a Passenger Counter App/15. The Document Object Model/1.15_index.js
+++ 1. Build a Passenger Counter App/15. The Document Object Model/1.15_index.js
@@ -2,8 +2,17 @@ let countEl = document.getElementById("count-el")
 let count = 0
 
 function increment() {
-    count = count + 1
+    count++
     countEl.innerText = count
 }
 
+/*
+The Document Object Model is how you use JS to modify a website.
 
+Document - interacting with a HTML document
+Object - document is of object data type, e.g., HTML document is a JS object
+Model - representation of elements, e.g.,
+
+HTML: <h2 id="count-el">0</h2>
+JS: let countEl = document.getElementById("count-el")
+ */
