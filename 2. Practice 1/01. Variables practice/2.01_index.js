8ec2efc1a914a430462d955469e492166da9b160
Evan Nartea <evannartea@Mac.lan>
2026-08-13 16:47:20 +0100

    finish 2.01

--- 2. Practice 1/01. Variables practice/2.01_index.js
+++ 2. Practice 1/01. Variables practice/2.01_index.js
@@ -1,7 +1,11 @@
 // Create two variables, firstName and lastName
+firstName = "Gio"
+lastName = "Nartea"
 
 // Concatenate the two variables into a third variable called fullName
+fullName = `${firstName} ${lastName}`
 
 // Log fullName to the console
+console.log(fullName)
 
 
