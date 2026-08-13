f76f376d2027d09e17c2fa43fb44e7970e234a6f
Evan Nartea <evannartea@Mac.lan>
2026-08-13 18:42:14 +0100

    finish 2.06

--- 2. Practice 1/06. Calculator challenge/2.06_index.js
+++ 2. Practice 1/06. Calculator challenge/2.06_index.js
@@ -1,14 +1,44 @@
 let num1 = 8
 let num2 = 2
+
 document.getElementById("num1-el").textContent = num1
 document.getElementById("num2-el").textContent = num2
+
+let sumEl = document.getElementById("sum-el")
 
 // Create four functions: add(), subtract(), divide(), multiply()
 // Call the correct function when the user clicks on one of the buttons
 // Perform the given calculation using num1 and num2
 // Render the result of the calculation in the paragraph with id="sum-el"
 
-// E.g. if the user clicks on the "Plus" button, you should render
+// E.g. if the user clicks on the "Add" button, you should render
 // "Sum: 10" (since 8 + 2 = 10) inside the paragraph with id="sum-el"
 
 
+function add() {
+    let addResult = num1 + num2;
+    sumEl.textContent = `Sum: ${addResult}`;
+
+    console.log(addResult);
+}
+
+function subtract() {
+    let subtractResult = num1 - num2;
+    sumEl.textContent = `Sum: ${subtractResult}`;
+
+    console.log(subtractResult);
+}
+
+function divide() {
+    let divideResult = num1 / num2;
+    sumEl.textContent = `Sum: ${divideResult}`;
+
+    console.log(divideResult);
+}
+
+function multiply() {
+    let multiplyResult = num1 * num2;
+    sumEl.textContent = `Sum: ${multiplyResult}`;
+
+    console.log(multiplyResult);
+}
