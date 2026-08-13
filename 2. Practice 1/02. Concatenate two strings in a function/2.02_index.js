253b58a6d302a55731fb0055e71ec464688d89b6
Evan Nartea <evannartea@Mac.lan>
2026-08-13 16:50:41 +0100

    finish 2.02

--- 2. Practice 1/02. Concatenate two strings in a function/2.02_index.js
+++ 2. Practice 1/02. Concatenate two strings in a function/2.02_index.js
@@ -1,5 +1,10 @@
-let name = "Linda"
+let name = "Gio"
 let greeting = "Hi there"
 
 // Create a function that logs out "Hi there, Linda!" when called
+function logGreeting() {
+    myGreeting = `${greeting}, ${name}!`
+    console.log(myGreeting)
+}
 
+logGreeting()
