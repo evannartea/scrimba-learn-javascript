551ca52f1b22fcb7e3b94e5ab6bd9c373068040f
Evan Nartea <evannartea@Mac.lan>
2026-08-13 12:16:36 +0100

    finish 1.23

--- 1. Build a Passenger Counter App/23. Improve the message with string concatenation/1.23_index.js
+++ 1. Build a Passenger Counter App/23. Improve the message with string concatenation/1.23_index.js
@@ -1,11 +1,14 @@
 let welcomeEl = document.getElementById("welcome-el")
 
-let name = "Per Harald Borgen"
-let greeting = "Welcome back "
+let name = "Gio"
+let greeting = "Welcome back"
 
-welcomeEl.innerText = greeting + name
+welcomeEl.innerText = `${greeting}, ${name}!`
 
-// Add an emoji to the end!
+// Add an emoji to the end! 🥸
 // WRITE YOUR CODE BELOW HERE
 // HINT: count = count + 1
 
+welcomeEl.innerText = `${welcomeEl.innerText} 🥸`
+
+// equivalent to welcomeEl.innerText += "🥸"
