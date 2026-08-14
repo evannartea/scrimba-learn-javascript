dad292146351e332c17a8806e7b172833d442ed5
Evan Nartea <evannartea@Mac.lan>
2026-08-14 13:18:51 +0100

    finish 3.16

--- 3. Build a Blackjack game/16. Display the cards/3.16_index.js
+++ 3. Build a Blackjack game/16. Display the cards/3.16_index.js
@@ -4,13 +4,16 @@ let sum = firstCard + secondCard
 let hasBlackJack = false
 let isAlive = true
 let message = ""
-let messageEl = document.getElementById("message-el")
-let sumEl = document.getElementById("sum-el")
-// 2. Store the cards paragraph in a variable called cardsEl
+let messageEl = document.querySelector("#message-el")
+let sumEl = document.querySelector("#sum-el")
+// Store the cards paragraph in a variable called cardsEl
+let cardsEl = document.querySelector("#cards-el")
 
 function startGame() {
-    // 3. Render the cars on the page using this format -> "Cards: 10 4"
-    sumEl.textContent = "Sum: " + sum
+    // Render the cards on the page using this format -> "Cards: 10 4"
+    cardsEl.textContent = `Cards: ${firstCard} ${secondCard}`
+    sumEl.textContent = `Sum: ${sum}`
+    
     if (sum <= 20) {
         message = "Do you want to draw a new card?"
     } else if (sum === 21) {
