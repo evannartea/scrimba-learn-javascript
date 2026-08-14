fb4cf0a16fcef27aaa18f10de88a0279d648941a
Evan Nartea <evannartea@Mac.lan>
2026-08-14 10:10:29 +0100

    finish 3.03

---
+++ 3. Build a Blackjack game/03. If...else conditionals/3.03_index.js
@@ -0,0 +1,13 @@
+let firstCard = 6
+let secondCard = 9
+let sum = firstCard + secondCard
+
+if (sum < 21) {
+    console.log("Do you want to draw a new card?")
+}
+else if (sum === 21) {
+    console.log("Blackjack!")
+}
+else (sum >  21) {
+    console.log("You're out of the game!")
+}
