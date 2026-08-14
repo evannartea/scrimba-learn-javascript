3fe498e5e657856905882dbf1bea9230d704c603
Evan Nartea <evannartea@Mac.lan>
2026-08-14 14:31:15 +0100

    finish 3.20

--- 3. Build a Blackjack game/20. Solving our cards problem with an array/3.20_index.js
+++ 3. Build a Blackjack game/20. Solving our cards problem with an array/3.20_index.js
@@ -1,12 +1,13 @@
 let firstCard = 10
 let secondCard = 4
+let cards = [firstCard, secondCard] // array - ordered list of items
 let sum = firstCard + secondCard
 let hasBlackJack = false
 let isAlive = true
 let message = ""
-let messageEl = document.getElementById("message-el")
-let sumEl = document.getElementById("sum-el")
-let cardsEl = document.getElementById("cards-el")
+let messageEl = document.querySelector("#message-el")
+let sumEl = document.querySelector("#sum-el")
+let cardsEl = document.querySelector("#cards-el")
 
 function startGame() {
     renderGame()
@@ -14,9 +15,9 @@ function startGame() {
 
 function renderGame() {
     // render out firstCard and secondCard
-    cardsEl.textContent = "Cards: " + firstCard + " " + secondCard
+    cardsEl.textContent = `Cards: ${cards[0]} ${cards[1]}`
     // render out ALL the cards we have
-    sumEl.textContent = "Sum: " + sum
+    sumEl.textContent = `Sum: ${sum}`
     if (sum <= 20) {
         message = "Do you want to draw a new card?"
     } else if (sum === 21) {
