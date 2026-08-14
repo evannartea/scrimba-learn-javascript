a89b012fadfda87688c4077948545438e68bd367
Evan Nartea <evannartea@Mac.lan>
2026-08-14 17:28:56 +0100

    finish 3.34

--- 3. Build a Blackjack game/34. Returning values in functions/3.34_index.js
+++ 3. Build a Blackjack game/34. Returning values in functions/3.34_index.js
@@ -5,19 +5,26 @@ let player2Time = 107
 function getFastestRaceTime() {
     if (player1Time < player2Time) {
         return player1Time
-    } else if (player2Time < player1Time) {
+    }
+    else if (player2Time < player1Time) {
         return player2Time
-    } else {
+    }
+    else {
         return player1Time
     }
 }
 
-// let fastestRace = getFastestRaceTime()
-// console.log(fastestRace)
+let fastestRace = getFastestRaceTime()
+console.log(fastestRace)
 
 // Write a function that returns the total race time
 // Call/invoke the function and store the returned value in a new variable
 // Finally, log the variable out
 
+function getTotalRaceTime() {
+    total = player1Time + player2Time
+    return total
+}
 
-
+let totalRace = getTotalRaceTime()
+console.log(totalRace)
