902c46714f54ba92d70c93c950cac84a2e9c7894
Evan Nartea <evannartea@Mac.lan>
2026-08-14 15:22:35 +0100

    finish 3.26

--- 3. Build a Blackjack game/26. Push a new card to the array/3.26_index.js
+++ 3. Build a Blackjack game/26. Push a new card to the array/3.26_index.js
@@ -1,22 +1,21 @@
 let firstCard = 10
 let secondCard = 4
-// 1. Create a new array - cards - that contains firstCard and secondCard
+let cards = [firstCard, secondCard]
 let sum = firstCard + secondCard
 let hasBlackJack = false
 let isAlive = true
 let message = ""
-let messageEl = document.getElementById("message-el")
-let sumEl = document.getElementById("sum-el")
-let cardsEl = document.getElementById("cards-el")
+let messageEl = document.querySelector("#message-el")
+let sumEl = document.querySelector("#sum-el")
+let cardsEl = document.querySelector("#cards-el")
 
 function startGame() {
     renderGame()
 }
 
 function renderGame() {
-    // 2. Refer to the cards array when rendering out the cards
-    cardsEl.textContent = "Cards: " + firstCard + " " + secondCard
-    sumEl.textContent = "Sum: " + sum
+    cardsEl.textContent = `Cards: ${cards[0]} ${cards[1]}`
+    sumEl.textContent = `Sum: ${sum}`
     if (sum <= 20) {
         message = "Do you want to draw a new card?"
     } else if (sum === 21) {
@@ -33,5 +32,8 @@ function renderGame() {
 function newCard() {
     let card = 6
     sum += card
+    // Push the card to the cards array
+    cards.push(card)
+    console.log(cards)
     renderGame()
 }
