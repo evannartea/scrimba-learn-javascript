c15be47e8fa2dbb053024e4ed0a0c20a3dc54e4e
Evan Nartea <evannartea@Mac.lan>
2026-08-16 13:18:53 +0100

    finish 3.46

--- 3. Build a Blackjack game/46. Write your first logical operator/3.46_index.js
+++ 3. Build a Blackjack game/46. Write your first logical operator/3.46_index.js
@@ -2,7 +2,10 @@ let hasSolvedChallenge = false
 let hasHintsLeft = false
 
 // Create an if statement that checks that both variables are false.
-// If so, run the showSolution() function
+if(hasSolvedChallenge && hasHintsLeft) {
+    // If so, run the showSolution() function
+    showSolution()
+}
 
 function showSolution() {
     console.log("Showing the solution....")
