be4be095a198b81832d88718955e9a692a42e0d7
Evan Nartea <evannartea@Mac.lan>
2026-08-16 16:10:20 +0100

    finish 4.01

---
+++ 4. Practice 2/01. Objects and functions/4.01_index.js
@@ -0,0 +1,18 @@
+// Create a person object that contains three keys: name, age, and country.
+// Use yourself as an example to set the values
+person = {
+    name: "Gio",
+    age: 26,
+    country: "UK"
+}
+
+// Create a function, logData(), that uses the person object to create a
+// string in the following format: 
+// "Per is 35 years old and lives in Norway"
+function logData() {
+    string = `${person.name} is ${person.age} years old and lives in the ${person.country}`
+    return string
+}
+
+// Call the logData() function to verify that it works
+console.log(logData())
