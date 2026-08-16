0d06127ae95f0b19ff5910b41f81c240fb14e6fd
Evan Nartea <evannartea@Mac.lan>
2026-08-16 16:42:25 +0100

    finish 4.05

--- 4. Practice 2/05. Logical operators/4.05_index.js
+++ 4. Practice 2/05. Logical operators/4.05_index.js
@@ -3,6 +3,10 @@ let weekday = "Friday"
 
 // If it is Friday the 13th, log out this spooky face: 😱
 // Use the logical "AND operator" -> &&
+function spookyFace() {
+    if (dayOfMonth === 13 && weekday === "Friday") {
+        return "😱"
+    }
+}
 
-
-
+console.log(spookyFace())
