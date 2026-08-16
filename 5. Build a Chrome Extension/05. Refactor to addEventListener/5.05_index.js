d716e9d25eb4d09df3202113c1510d00faf32bed
Evan Nartea <evannartea@Mac.lan>
2026-08-16 17:44:44 +0100

    finish 5.05

---
+++ 5. Build a Chrome Extension/05. Refactor to addEventListener/5.05_index.js
@@ -0,0 +1,15 @@
+let inputBtn = document.querySelector("#input-btn")
+
+inputBtn.addEventListener("click", function() {
+    console.log("Button clicked!")
+})
+
+/* 
+same as the following:
+
+<button id="input-btn" onclick="saveLead()">SAVE INPUT</button>
+
+function saveLead() {
+    console.log("Button clicked!")
+}
+*/
