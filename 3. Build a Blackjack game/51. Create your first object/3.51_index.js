398ad297b143388bfab899d84dc25eb6efe45cc0
Evan Nartea <evannartea@Mac.lan>
2026-08-16 13:54:09 +0100

    finish 3.51

---
+++ 3. Build a Blackjack game/51. Create your first object/3.51_index.js
@@ -0,0 +1,12 @@
+// Create an object that represents an airbnb castle listing.
+let castle = {
+    // It should contain at least one boolean, one string, one number, and one array
+    isAvailable: true,
+    name: "King Arthur's Castle",
+    price: 200000,
+    tags: ["poop", "fart"]
+}
+
+// Log out at least two of the keys using the dot notation
+console.log(castle.isAvailable)
+console.log(castle.price)
