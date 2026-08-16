615895cf138c23f682caf3283723f145c1fb4483
Evan Nartea <evannartea@Mac.lan>
2026-08-16 13:22:13 +0100

    finish 3.47

--- 3. Build a Blackjack game/47. The logical OR operator/3.47_index.js
+++ 3. Build a Blackjack game/47. The logical OR operator/3.47_index.js
@@ -1,5 +1,11 @@
 // Create two boolean variables, likesDocumentaries and likesStartups
+let likesDocumentaries = true
+let likesStartups = true
+
 // Use an OR statement (||) to call recommendMovie() if either of those variables are true
+if (likesDocumentaries || likesStartups) {
+    recommendMovie()
+}
 
 function recommendMovie() {
     console.log("Hey, check out this new film we think you will like!")
