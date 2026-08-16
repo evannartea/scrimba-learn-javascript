5578e4b98d1354bfea248b38a2539937e0e56cca
Evan Nartea <evannartea@Mac.lan>
2026-08-16 17:54:57 +0100

    finish 5.06

---
+++ 5. Build a Chrome Extension/06. Write your first addEventListener()/5.06_index.js
@@ -0,0 +1,8 @@
+// Grab the box from the DOM and store it in a variable
+let box = document.querySelector("#box")
+
+// Add a click event listener to the box
+box.addEventListener("click", function() {
+    // Log out "I want to open the box!" when it's clicked
+    console.log("I want to open the box!")
+})
