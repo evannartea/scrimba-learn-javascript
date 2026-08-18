217dd29ac184f9d3d792085af95b502c21f65764
Evan Nartea <evannartea@Mac.lan>
2026-08-18 11:38:08 +0100

    finish 5.17

---
+++ 5. Build a Chrome Extension/17.  More innerHTML practice/5.17_index.js
@@ -0,0 +1,9 @@
+const container = document.querySelector("#container")
+
+container.innerHTML = `<button onclick="buy()">Buy!</button>`
+
+// When clicked, render a paragraph under the button (in the container)
+// that says "Thank you for buying!"
+function buy() {
+    container.innerHTML += `<p>Thanks for buying!</p>`
+}
