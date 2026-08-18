192103676d03395cfd86e29646a5514743adb6af
Evan Nartea <evannartea@Mac.lan>
2026-08-18 10:56:04 +0100

    finish 5.13

--- 5. Build a Chrome Extension/13. Create the unordered list/5.13_index.js
+++ 5. Build a Chrome Extension/13. Create the unordered list/5.13_index.js
@@ -2,7 +2,7 @@ let myLeads = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"]
 const inputEl = document.querySelector("#input-el")
 const inputBtn = document.querySelector("#input-btn")
 // Grab the unordered list and store it in a const variable called ulEl
-
+const ulEl = document.querySelector("#ul-el")
 
 inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value)
