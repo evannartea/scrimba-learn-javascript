c957a236e116754f755beab144102c4ed042bbc0
Evan Nartea <evannartea@Mac.lan>
2026-08-20 14:14:44 +0100

    finish 6.03

---
+++ 6. Practice 3/03. save to localStorage/6.03_index.js
@@ -0,0 +1,8 @@
+// Save a value to localStorage
+//localStorage.setItem("key", "string")
+
+// Delete your code and refresh the page
+
+// Fetch your value from localStorage and log it out
+localStorageValue = localStorage.getItem("key")
+console.log(localStorageValue)
