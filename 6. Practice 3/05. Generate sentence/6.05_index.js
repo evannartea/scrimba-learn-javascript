7400da52834e32520a4cdcd1e7cedfa6debcd55d
Evan Nartea <evannartea@Mac.lan>
2026-08-20 14:43:12 +0100

    finish 6.05

--- 6. Practice 3/05. Generate sentence/6.05_index.js
+++ 6. Practice 3/05. Generate sentence/6.05_index.js
@@ -4,7 +4,18 @@
 
 // Use both a for loop and a template string to solve the challenge
 function generateSentence(desc, arr) {
-    
+    let string = `The ${arr.length} ${desc} are `
+
+    for (i = 0; i < arr.length; i++) {
+        if (i === arr.length - 1) {
+            string += arr[i]
+        }
+        else {
+            string += `${arr[i]}, `
+        }
+    }
+
+    return string
 }
 
 // Example 1: if you pass in "largest countries",and ["China", "India", "USA"],
@@ -14,4 +25,5 @@ console.log(largestCountries)
 
 // Example 2:If you pass in "best fruits" and ["Apples", "Bananas"], it should return:
 // "The 2 best fruits are Apples, Bananas"
-generateSentence("best fruits", ["Apples", "Bananas"])
+let bestFruits = generateSentence("best fruits", ["Apples", "Bananas"])
+console.log(bestFruits)
